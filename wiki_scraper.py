139a906667d4ae2ce0fda1efc918057c7d9bc98e
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:16:08 -0500

    Update wiki_scraper.py

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -185,18 +185,5 @@ class Event:
         self.description: str = data.get("description", "")
         self.scrape_date: datetime = data.get("scrape_date", datetime.now())
 
-if __name__ == "__main__": #marcador de posición
-    scraper = WebScraper("https://tambinsoyunal.fandom.com/es/wiki/Universidad_Nacional_de_Colombia")
-    titulos = scraper.extract_titles()
-    parrafos = scraper.extract_paragraphs()
-
-    print("=== TÍTULOS ===")
-    for t in titulos:
-        print("-", t)
-
-    print("\n=== PÁRRAFOS (primeros 5) ===")
-    for p in parrafos[:5]:
-        print(p, "\n")
-
 
 
