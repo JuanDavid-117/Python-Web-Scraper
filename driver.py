af448b42ace958e5c556106ac841a9143fd5f9b9
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:38:57 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Quita si quieres ver el navegador
+
+    service = Service("chromedriver.exe")  # Ajusta la ruta a donde tengas el driver
+
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
