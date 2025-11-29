a30c4ce11576b1d0c57efea5906aaebfbf0181fa
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:31:53 -0500

    Update and rename drivers.py to driver.py

--- driver.py
+++ driver.py
@@ -1,7 +1,3 @@
-import requests # para hacer peticiones HTTP SIN abrir un navegador real
-from bs4 import BeautifulSoup # es para parsear HTML (limpiarlo, leerlo, buscar elementos).
-from datetime import datetime, date #Para manejar fechas
-from typing import List #Sirve para dar tipos en Python:
 from selenium import webdriver #Esto inicializa el navegador (Chrome, Firefox, etc.).
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
