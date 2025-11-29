a20bc5368c9a27b3d8837f91bb6003102114b3cf
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:32:18 -0500

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,4 @@
+import requests # para hacer peticiones HTTP SIN abrir un navegador real
+from bs4 import BeautifulSoup # es para parsear HTML (limpiarlo, leerlo, buscar elementos).
+from datetime import datetime, date #Para manejar fechas
+from typing import List #Sirve para dar tipos en Python:
