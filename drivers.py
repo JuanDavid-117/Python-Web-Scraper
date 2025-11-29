2d7fc8acc1329059380a9b783366a1a45d3edb41
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:10:04 -0500

    Update drivers.py

--- drivers.py
+++ drivers.py
@@ -1,8 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, date
-from typing import List
-from selenium import webdriver
+import requests # para hacer peticiones HTTP SIN abrir un navegador real
+from bs4 import BeautifulSoup # es para parsear HTML (limpiarlo, leerlo, buscar elementos).
+from datetime import datetime, date #Para manejar fechas
+from typing import List #Sirve para dar tipos en Python:
+from selenium import webdriver #Esto inicializa el navegador (Chrome, Firefox, etc.).
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
