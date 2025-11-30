b518491410f4dcb5e856ee0438eafb4b8ece7f7e
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 20:15:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ from imports import *
 from scraper import WebScraper
 
 if __name__ == "__main__": #marcador de posición
-    scraper = WebScraper("https://tambinsoyunal.fandom.com/es/wiki/Universidad_Nacional_de_Colombia")
+    scraper = WebScraper("https://bienestar.bogota.unal.edu.co/eventos.php")
     titulos = scraper.extract_titles()
     parrafos = scraper.extract_paragraphs()
-
-    print("=== TÍTULOS ===")
-    for t in titulos:
-        print("-", t)
-
-    print("\n=== PÁRRAFOS (primeros 5) ===")
-    for p in parrafos[:5]:
-        print(p, "\n")
